1e3341a58774fcdccae973b3dfd6f3b4dc551655
jHetzer <jonathan.hetzer95@gmail.com>
2019-12-06 14:01:34 +0100

    Style: PEP 8 -- Style Guide (Round 1)

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-# noqa: D100
+
 from setuptools import setup, find_packages
-import re, ast
+import re
+import ast
 
 with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+    install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in erpnextfints/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('erpnextfints/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='erpnextfints',
-	version=version,
-	description='FinTS Connector for ERPNext (Germany)',
-	author='jHetzer',
-	author_email='jHetzer@users.noreply.github.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[
-		"install_requires",
-        "fints @ git+https://github.com/raphaelm/python-fints.git@psd2#egg=fints",
+    name='erpnextfints',
+    version=version,
+    description='FinTS Connector for ERPNext (Germany)',
+    author='jHetzer',
+    author_email='jHetzer@users.noreply.github.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        "install_requires",
+        "fints @ git+https://github.com/raphaelm/python-fints.git@psd2#egg=fints",  # noqa: E501
     ],
 )

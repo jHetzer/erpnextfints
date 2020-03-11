29c163ea5336ceffc82dd20fc336caa27e33328c
jHetzer <jonathan.hetzer95@gmail.com>
2020-03-11 22:12:18 +0100

    Feat: (BAW) Check permission to create bank/bank account doctype
    By default only the role "System Manager" is allowed to create Banks in
    ERPNext.
    The "Bank Account Wizard" page is only accessible for "Account Manager"
    and "Account User" roles. By this fix any BAW user will by able to create
    "Bank" doctypes

--- erpnextfints/utils/bank_account_controller.py
+++ erpnextfints/utils/bank_account_controller.py
@@ -25,15 +25,37 @@ class BankAccountController:
         :return: Dict with status and bank details
         """
         try:
+            ignore_permissions = False
             payment_doc = json.loads(payment_doc)
-            bankData = json.loads(bankData)
 
+            if has_page_permission('bank_account_wizard'):
+                ignore_permissions = True
+            else:
+                raise PermissionError(_(
+                    'User {0} does not have access to this method'
+                ).format(frappe.bold(frappe.session.user)))
+
+            # Check for FinTS Login default parties
+            if frappe.get_all(
+                'FinTS Login',
+                or_filters=[
+                    ['default_customer', '=', payment_doc.get('party')],
+                    ['default_supplier', '=', payment_doc.get('party')]
+                ],
+                ignore_permissions=ignore_permissions
+            ):
+                raise ValueError(_(
+                    'Default party selected, please change party'
+                ))
+
+            bankData = json.loads(bankData)
             bankName = bankData.get('name')
+
             if not frappe.db.exists('Bank', {'bank_name': bankName}):
                 frappe.get_doc({
                     'doctype': 'Bank',
                     'bank_name': bankName}
-                ).insert()
+                ).insert(ignore_permissions=ignore_permissions)
 
             gl_account = None
             if payment_doc.get('payment_type') == "Pay":
@@ -59,8 +81,11 @@ class BankAccountController:
                 bankData.get('name')
             ])
             if not frappe.db.exists('Bank Account', bankAccountName):
-                newBankAccount = frappe.get_doc(bankAccount)
-                newBankAccount.insert()
+                newBankAccount = frappe.get_doc(
+                    bankAccount,
+                    ignore_permissions=ignore_permissions
+                )
+                newBankAccount.insert(ignore_permissions)
                 frappe.msgprint(_("Successfully created Bank Account"))
                 return {"bankAccount": newBankAccount, "status": True}
             else:
@@ -87,6 +112,32 @@ class BankAccountController:
         sqlQuery = filehandle.read()
         filehandle.close()
         return frappe.db.sql(sqlQuery, as_dict=True)
+
+
+def has_page_permission(page_name):
+    """Check if user has permission for a page doctype.
+
+    Based on frappe/desk/desk_page.py
+    :param page_doc: page doctype object
+    :type page_doc: page doctyp
+    :return: Boolean
+    """
+    if (
+        frappe.session.user == 'Administrator'
+        or 'System Manager' in frappe.get_roles()
+    ):
+        return True
+
+    page_doc = frappe.get_doc('Page', page_name)
+    page_roles = [d.role for d in page_doc.get('roles')]
+    if page_roles:
+        if frappe.session.user == 'Guest' and 'Guest' not in page_roles:
+            return False
+        elif set(page_roles).intersection(set(frappe.get_roles())):
+            # check if roles match
+            return True
+    else:
+        return False
 
 
 def validate_unique_iban(doc, method):

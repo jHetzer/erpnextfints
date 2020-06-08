4ee9deb8442894cca64927383c4387f9bac603ee
jHetzer <jonathan.hetzer95@gmail.com>
2020-06-08 21:16:08 +0200

    Revert "pre release/0.4.1"

--- erpnextfints/public/js/controllers/fints_interactive.js
+++ erpnextfints/public/js/controllers/fints_interactive.js
@@ -5,32 +5,12 @@ frappe.provide("erpnextfints.interactive");
 
 erpnextfints.interactive = {
 	progressbar: function(frm) {
-		let ajaxErrorCloseHandler = function(/* e */){
-			// Queue close function on ajax error
-			setTimeout(function(){
-				frappe.hide_progress();
-			},2000);
-		};
-		let pageUnloadHandler = function(){
-			$(document).unbind("ajaxError",ajaxErrorCloseHandler);
-			setTimeout(function(){
-				// Queue unbind action on page leave
-				$(document).unbind("form-unload",pageUnloadHandler);
-			},0);
-		};
-		frappe.ui.form.on(frm.doc.doctype, {
-			refresh: function() {
-				$(document).ajaxError(ajaxErrorCloseHandler);
-			}
-		});
-		$(document).on("form-unload", pageUnloadHandler);
-
 		frappe.realtime.on("fints_progressbar", function(data) {
 			if(data.docname === frm.doc.name) {
 				if(data.reload && data.reload === true) {
 					frm.reload_doc();
 				}
-				if(data.progress === 100) {
+				if(data.progress==100) {
 					frappe.hide_progress();
 				} else {
 					frappe.show_progress(data.docname,data.progress,100,data.message);
@@ -38,4 +18,4 @@ erpnextfints.interactive = {
 			}
 		});
 	},
-};
+}
